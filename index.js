bfcecafc54d497cab45d2d8b44da119350d3f879
jamilagastar <jamilagastar@gmail.com>
2022-04-20 12:45:48 +1000

    completed lab

---
+++ index.js
@@ -0,0 +1,17 @@
+let divide = function() {
+    return 2000 / 100;
+}
+
+console.log(divide());
+
+//let square = function(x) {
+//    return x ** 2;
+//}
+//
+//console.log(square(4));
+
+let square = (x) => x ** 2;
+console.log(square(4));
+
+let add = (num1, num2) => num1 + num2;
+console.log(add(6,6));
